08e0340d89db2c46cabb4593521fee7d294e9a11
louis-coding123 <louis.szwarcberg@gmail.com>
2017-04-12 15:55:47 +0800

    Jvscript edit

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,39 +1,104 @@
 $(document).on("pagecreate","#rockPaperScissors",function(event){
   
-
+//vars for images
 var rock=document.getElementById("Rock");
 var paper=document.getElementById("Paper");
 var scissors=document.getElementById("Scissors");
+
+//var for reset button
 var reset=document.getElementById("reset");
-var choices=document.getElementById("")
+
+//var for player span
 var points=document.getElementById("playerScore");
+
+//var for computer span
+var compPoints=document.getElementById("compScore");
+
+//var for scores
 var score = 0;
+var compScore = 0;
+
+//var for result text that is displayed
 var result = document.getElementById("result");
 
+function getCompchoice(){
+var choices=['rock','paper','scissors'];
+var compChoice=choices[Math.floor(Math.random()*choices.length)]
+return compChoice;
+}
+
 rock.addEventListener('click',function(){
-  	score=score+1;
-  	points.innerHTML=score;
-  	result.innerHTML = 'You Picked Rock. Computer Picked ... You Lost/Won';
+
+var computerChoice = getCompchoice();
+
+if(computerChoice === "scissors"){
+score=score+1;
+points.innerHTML=score;
+result.innerHTML = 'You Picked Rock. Computer Picked Scissors. You Win!!';
+}
+
+else if(computerChoice === "paper") {
+ compScore = compScore + 1;
+ compPoints.innerHTML = compScore;
+ result.innerHTML = 'You Picked Rock. Computer Picked Paper. You Lose!!'
+}
+
+else {
+    result.innerHTML = 'You Picked Rock. Computer Picked Rock. Its A Tie!'
+}
+
 });
 
 
 reset.addEventListener('click',function(){  
 	score=0;
 	points.innerHTML=score;
-	result.innerHTML = '';
+	compScore=0
+  compPoints.innerHTML=score
+  result.innerHTML = '';
 });
 
-paper.addEventListener('click',function(){
- 	score=score+1;
-  	points.innerHTML=score;
-  	result.innerHTML = 'You Picked Paper. Computer Picked ... You Lost/Won';
-});
+// paper.addEventListener('click',function(){
+//  	var computerChoice = getCompchoice();
 
-scissors.addEventListener('click',function(){
-  	score=score+1;
-  	points.innerHTML=score;
-  	result.innerHTML = 'You Picked Scissors. Computer Picked ... You Lost/Won';
-});
+// if(computerChoice === "scissors"){
+// score=score-1;
+// points.innerHTML=score;
+// result.innerHTML = 'You Picked Paper. Computer Picked Scissors. You Lose!!';
+// }
+
+// else if(computerChoice === "Paper") {
+
+//     result.innerHTML = 'You Picked Paper. Computer Picked Paper. Its A Tie!!'
+// }
+
+// else {
+//      score=score+1;
+//  points.innerHTML=score;
+//     result.innerHTML = 'You Picked Paper. Computer Picked Rock. You Win!!'
+// }
+// });
+
+// scissors.addEventListener('click',function(){
+//   	var computerChoice = getCompchoice();
+
+// if(computerChoice === "scissors"){
+
+// result.innerHTML = 'You Picked Scissors. Computer Picked Scissors. Its A Tie!!';
+// }
+
+// else if(computerChoice === "Paper") {
+//  score=score+1;
+//  points.innerHTML=score;
+//     result.innerHTML = 'You Picked Scissors. Computer Picked Paper. You Win!!'
+// }
+
+// else {
+//     result.innerHTML = 'You Picked Scissors. Computer Picked Rock. You Lose!!'
+//      score=score-1;
+//  points.innerHTML=score;
+// }
+// });
 
 
 
