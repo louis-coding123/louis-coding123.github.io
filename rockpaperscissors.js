e16c9048e81a0ba87273c8501389c27ee93d0a35
louis-coding123 <louis.szwarcberg@gmail.com>
2017-04-12 13:58:43 +0800

    RPS Text add

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -5,29 +5,34 @@ var rock=document.getElementById("Rock");
 var paper=document.getElementById("Paper");
 var scissors=document.getElementById("Scissors");
 var reset=document.getElementById("reset");
-
+var choices=document.getElementById("")
 var points=document.getElementById("playerScore");
 var score = 0;
+var result = document.getElementById("result");
 
 rock.addEventListener('click',function(){
   	score=score+1;
   	points.innerHTML=score;
+  	result.innerHTML = 'You Picked Rock. Computer Picked ... You Lost/Won';
 });
 
 
 reset.addEventListener('click',function(){  
 	score=0;
 	points.innerHTML=score;
+	result.innerHTML = '';
 });
 
 paper.addEventListener('click',function(){
  	score=score+1;
   	points.innerHTML=score;
+  	result.innerHTML = 'You Picked Paper. Computer Picked ... You Lost/Won';
 });
 
 scissors.addEventListener('click',function(){
   	score=score+1;
   	points.innerHTML=score;
+  	result.innerHTML = 'You Picked Scissors. Computer Picked ... You Lost/Won';
 });
 
 
